328c8af85f30f9d8d8bcb57b45f57205301442cf
Hunterfzh <faaizah.afoda64@myhunter.cuny.edu>
2026-05-05 00:47:47 -0400

    my first server files

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-// Your server code goes here. Host your server on localhost:3000
+const http = require("http"); //install depedencies https 
+const fs = require("fs");  //install depedencies fs 
+
+const server = http.createServer((req, res) => {
+  fs.readFile("index.html", (err,data)=> {
+    res.end(data);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running at http://localhost:3000"); //see if it works 
+});
+
+
